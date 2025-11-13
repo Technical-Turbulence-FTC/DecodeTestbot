805a5043e3809ceeb8e811d48cc96a04ef09ff1c
KeshavAnandCode <keshavanandofficial@gmail.com>
2025-11-12 22:01:12 -0600

    Boilerplate

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libs/blank.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.libs;
+
+public class blank {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Robot.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.utils;
+
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+public class Robot {
+
+    //Initialize public motors, servos, components here:
+
+
+
+//    public DcMotorEx shooter1;
+//    public DcMotorEx shooter2;
+//
+//
+
+    public Robot (HardwareMap hardwareMap) {
+
+        //Define components w/ hardware map
+
+//
+//        shooter1 = hardwareMap.get(DcMotorEx.class, "shooter1");
+//
+//        shooter2 = hardwareMap.get(DcMotorEx.class, "shooter2");
+//
+//
+
+
+
+
+    }
+}
